1f8654c5d3ab0c843d475f316a835bd64164f079
yuwei123-test <l>
2020-06-07 09:16:25 +0800

    登录页完成

--- public/mymanage/js/login.js
+++ public/mymanage/js/login.js
@@ -20,6 +20,9 @@ $(function(){
             max: 6,
             message: '用户名长度必须在2到6位之间'
           },
+          callback: {
+            message:'用户名不存在'
+          }
         
         }
       },
@@ -33,7 +36,9 @@ $(function(){
             max: 12,
             message: '密码长度必须在2到6位之间'
           },
-        
+          callback : {
+            message: '密码错误'
+          }
         }  
       }
     }
@@ -50,8 +55,26 @@ $(function(){
      dataType: 'json',
      success:function(info){
       console.log(info);
-      
+      if(info.success){
+        // alert('登录成功')
+        location.herf = 'index.html'
+      }
+      if(info.error==1000){
+        // alert('用户名不存在')
+        $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback')
+      }
+      if(info.error==1001){
+        // alert('密码错误')
+        $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback')
+      }
      }
    })
   });
+  //重置功能实现
+  $('[type="reset"]').click(function(){
+    console.log(111);
+    //除了要重置文本还要重置校验状态
+    $('#form').data('bootstrapValidator').resetForm();
+    
+  })
 })
